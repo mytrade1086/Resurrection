6e991aff607ed1c098824c2dcf242b06464d62aa
sunshine <mytrade1086@gmail.com>
2020-04-09 20:08:16 +0530

    Generic method

---
+++ src/practice/java/GenericMethodDemo.java
@@ -0,0 +1,23 @@
+package practice.java;
+
+public class GenericMethodDemo {
+	
+//#Problem: Loop through Array of any data type( except primitive)
+//Solun 1: Overload methos with String Array,Integer Array
+//Solun2 : Generic method . Observe E before return type and Array type.
+	
+	public <E>void loopArray(E[]str) {	
+		for(int i=0;i<str.length;i++) {
+			System.out.println(str[i]);		
+		}			
+	}
+	
+	
+	public static void main(String[] args) {	
+		GenericMethodDemo gmd=new GenericMethodDemo();
+		gmd.loopArray(new String[] {"sumit","neha","shanu","mac"});
+		gmd.loopArray(new Integer[] {1,2,4,5,10});
+		
+	}
+
+}
