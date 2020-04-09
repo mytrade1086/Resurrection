5ddf8d291cc9b4a854de8d729bc0d02c98eb7535
sunshine <mytrade1086@gmail.com>
2020-04-09 20:27:07 +0530

    genericMethod with for each

--- src/practice/java/GenericMethodDemo.java
+++ src/practice/java/GenericMethodDemo.java
@@ -1,23 +1,46 @@
 package practice.java;
 
 public class GenericMethodDemo {
-	
-//#Problem: Loop through Array of any data type( except primitive)
-//Solution 1: Overload methods with String Array,Integer Array
-//Solun2 : Generic method . Observe E before return type and Array type.
-	
-	public <E>void loopArray(E[]str) {	
-		for(int i=0;i<str.length;i++) {
-			System.out.println(str[i]);		
-		}			
+
+//#Problem: Loop through Array of any data type(except primitive)
+//Solution 1 PolyMorphism: Overload methods with String Array,Integer Array
+//Solution2 : Generic method . Observe E before return type and Array type.
+
+	public <E> void loopArray(E[] str) {
+		for (int i = 0; i < str.length; i++) {
+			System.out.println(str[i]);
+		}
 	}
 	
-	
-	public static void main(String[] args) {	
-		GenericMethodDemo gmd=new GenericMethodDemo();
-		gmd.loopArray(new String[] {"sumit","neha","shanu","mac"});
-		gmd.loopArray(new Integer[] {1,2,4,5,10});
+	public <E> void loopArrayForEach(E[] str) {
+		System.out.println("Printing with for each loop");
+		for (E x : str) {
+			System.out.println(x);
+		}
+	}
+
+	public static void main(String[] args) {
+		GenericMethodDemo gmd = new GenericMethodDemo();
+		gmd.loopArray(new String[] { "sumit", "neha", "shanu", "mac" });	
+//		sumit
+//		neha
+//		shanu
+//		mac
 		
+		gmd.loopArray(new Integer[] { 1, 2, 4, 5, 10 });
+//		1
+//		2
+//		4
+//		5
+//		10	
+		
+		gmd.loopArrayForEach(new Integer[] { 100, 200, 4, 5, 10 });
+//		Printing with for each loop
+//		100
+//		200
+//		4
+//		5
+//		10
 	}
 
 }
